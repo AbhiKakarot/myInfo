148320a0ba8c5fb5708734302e64bf24a14cb065
abhishek <munjalabhishek1996@gmail.com>
2020-10-10 11:17:12 +0530

    Mobile Compatible design

--- static/js/myown.js
+++ static/js/myown.js
@@ -1,6 +1,98 @@
 AOS.init();
 
 
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".carousel-container").forEach((carousel) => {
+    insertNumbers(carousel);
+
+    carousel.querySelector(".prev").addEventListener("click", (e) => {
+      minusItem(carousel);
+    });
+
+    carousel.querySelector(".next").addEventListener("click", () => {
+      plusItem(carousel);
+    });
+
+    insertDots(carousel);
+
+    carousel.querySelectorAll(".dot").forEach((dot) => {
+      dot.addEventListener("click", (e) => {
+        let item = Array.prototype.indexOf.call(
+          e.target.parentNode.children,
+          e.target
+        );
+
+        showItems(carousel, item);
+      });
+    });
+
+    showItems(carousel, 0);
+  });
+});
+
+function insertNumbers(carousel) {
+  const length = carousel.querySelectorAll(".item").length;
+  for (let i = 0; i < length; i++) {
+    const nmbr = document.createElement("div");
+    nmbr.classList.add("numbertext");
+    nmbr.innerText = i + 1 + " / " + length;
+
+    carousel.querySelectorAll(".item")[i].append(nmbr);
+  }
+}
+
+function insertDots(carousel) {
+  const dots = document.createElement("div");
+  dots.classList.add("dots");
+
+  carousel.append(dots);
+
+  carousel.querySelectorAll(".item").forEach((elem) => {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+
+    carousel.querySelector(".dots").append(dot);
+  });
+}
+
+function plusItem(carousel) {
+  let item = currentItem(carousel);
+
+  carousel
+    .querySelectorAll(".item")
+    [item].nextElementSibling.classList.contains("item")
+    ? showItems(carousel, item + 1)
+    : showItems(carousel, 0);
+}
+
+function minusItem(carousel) {
+  let item = currentItem(carousel);
+
+  carousel.querySelectorAll(".item")[item].previousElementSibling != null
+    ? showItems(carousel, item - 1)
+    : showItems(carousel, carousel.querySelectorAll(".item").length - 1);
+}
+
+function currentItem(carousel) {
+  return [...carousel.querySelectorAll(".item")].findIndex(
+    (item) => item.style.display == "block"
+  );
+}
+
+function showItems(carousel, item) {
+  if (carousel.querySelectorAll(".item")[currentItem(carousel)] != undefined)
+    carousel.querySelectorAll(".item")[currentItem(carousel)].style.display =
+      "none";
+  carousel.querySelectorAll(".item")[item].style.display = "block";
+
+  if (carousel.querySelector(".dot.active") != null)
+    carousel.querySelector(".dot.active").classList.remove("active");
+  carousel.querySelectorAll(".dot")[item].classList.add("active");
+}
+
+
+
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("#next").on('click', function(event) {
@@ -30,8 +122,7 @@ $(document).ready(function(){
   });
 });
 
-function scrollToContact(){
-  var hash = '#sec4';
+function scrollToSection(hash){
   $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
@@ -67,11 +158,11 @@ app = new Vue({
       vMessage: '',
 
       socialIcons: [
-      {social: 'fab fa-facebook', url: '#'},
-      {social: 'fab fa-instagram', url: '#'},
-      {social: 'fab fa-linkedin', url: 'https://www.linkedin.com/in/abhishek-munjal-619017a9/'},
-      {social: 'fab fa-github', url: 'https://github.com/AbhiKakarot/'},
-      {social: 'far fa-envelope', url: 'mailto:munjalabhishek1996@gmail.com'},
+      {social: 'fab fa-facebook', url: '#', target:''},
+      {social: 'fab fa-instagram', url: '#', target:''},
+      {social: 'fab fa-linkedin', url: 'https://www.linkedin.com/in/abhishek-munjal-619017a9/',target:'_blank'},
+      {social: 'fab fa-github', url: 'https://github.com/AbhiKakarot/',target:'_blank'},
+      {social: 'far fa-envelope', url: 'mailto:munjalabhishek1996@gmail.com',target:'_blank'},
       ],
 
       greeting: [
@@ -110,5 +201,16 @@ app = new Vue({
     window.setInterval(()=>{
       this.greet();
     }, 3500);
+
+    /*var this_kw = this;
+    $(document).ready(function(){
+      var labels = document.getElementsByClassName('collapse-right');
+      console.log(labels);
+      for(var i=1;i<labels.length;i++){
+        $(labels[i]).siblings().hide();
+        $(labels[i]).removeClass('collapse-right');
+        $(labels[i]).addClass('collapse-down');  
+      }
+    });*/
   }
  });

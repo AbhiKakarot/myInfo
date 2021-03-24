9521ad8a366e66c2dc0fe29239e2fa710b252a78
abhishek <munjalabhishek1996@gmail.com>
2021-03-24 11:31:43 +0530

    Redesigned start page, changed some backgrounds, updated a few written stuff

--- static/js/myown.js
+++ static/js/myown.js
@@ -159,8 +159,8 @@ app = new Vue({
 
       socialIcons: [
       {social: 'fas fa-file', url: 'https://drive.google.com/file/d/14CNGW8TiQN-z8pLdPbpsz6_nfG6BNa0i/view?usp=sharing', target: '_blank'},
-      {social: 'fab fa-facebook', url: '#open-modal', target:''},
-      {social: 'fab fa-instagram', url: '#open-modal', target:''},
+      //{social: 'fab fa-facebook', url: '#open-modal', target:''},
+      {social: 'fab fa-instagram', url: 'https://instagram.com/abhishek__munjal/', target:''},
       {social: 'fab fa-linkedin', url: 'https://www.linkedin.com/in/abhishek-munjal-619017a9/',target:'_blank'},
       {social: 'fab fa-github', url: 'https://github.com/AbhiKakarot/',target:'_blank'},
       {social: 'far fa-envelope', url: 'mailto:munjalabhishek1996@gmail.com',target:'_blank'},
